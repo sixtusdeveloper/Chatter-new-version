e5dc07ef05dbb104b67973c701f749cb91b6327f
sixtusdeveloper <sixtusushrey@gmail.com>
2024-08-15 21:05:39 +0100

    commit on firebaseConfig file

--- firebase/firebaseConfig.ts
+++ firebase/firebaseConfig.ts
@@ -1,10 +1,9 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
-import { getAnalytics } from 'firebase/analytics';
-import { getAuth } from 'firebase/auth';  // Import getAuth from Firebase Authentication
+import { getAnalytics, isSupported } from 'firebase/analytics';
+import { getAuth } from 'firebase/auth';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBkfzxAMgi9d7n2HsO1R5QmkY3O3_AQAbA",
   authDomain: "chatter-application-549bd.firebaseapp.com",
@@ -17,7 +16,16 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const firebaseApp = initializeApp(firebaseConfig);
-const analytics = getAnalytics(firebaseApp);
+
+// Conditionally initialize Firebase Analytics
+let analytics;
+if (typeof window !== "undefined") {
+  isSupported().then((supported) => {
+    if (supported) {
+      analytics = getAnalytics(firebaseApp);
+    }
+  });
+}
 
 // Initialize Firebase Authentication and export it
 export const auth = getAuth(firebaseApp);
