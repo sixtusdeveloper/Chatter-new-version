82d7c1ce4015ac31b37f0f971d8a2071383242cf
sixtusdeveloper <sixtusushrey@gmail.com>
2024-12-08 19:05:42 +0100

    updated the navbar component and home page for the sign in button

--- app/pages/about/page.tsx
+++ app/pages/about/page.tsx
@@ -18,31 +18,25 @@ import { SignedIn, SignedOut, RedirectToSignIn } from "@clerk/nextjs";
 
 export default function About() {
   return (
-    <>
-      <SignedIn>
-        <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
-          <div className="max-w-7xl w-screen">
-            <SingleNav navigation={aboutNavigation} />
-            <AboutIntroduction />
-            <AboutMission />
-            <AboutVision />
-            <AboutFeatures />
-            <AboutTeam />
-            <AboutStory />
-            <Testimonials />
-            <AboutCTA />
-            <AboutFooter />
-            <Script
-              src="https://embed.tawk.to/66b0c4651601a2195ba0f54b/1i4h7ub94"
-              charset="UTF-8"
-              crossorigin="*"
-            />
-          </div>
-        </main>
-      </SignedIn>
-      <SignedOut>
-        <RedirectToSignIn />
-      </SignedOut>
-    </>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
+      <div className="max-w-7xl w-screen">
+        <SingleNav navigation={aboutNavigation} />
+        <AboutIntroduction />
+        <AboutMission />
+        <AboutVision />
+        <AboutFeatures />
+        <AboutTeam />
+        <AboutStory />
+        <Testimonials />
+        <AboutCTA />
+        <AboutFooter />
+        <Script
+          src="https://embed.tawk.to/66b0c4651601a2195ba0f54b/1i4h7ub94"
+          charset="UTF-8"
+          crossorigin="*"
+        />
+      </div>
+    </main>
+
   );
 }
