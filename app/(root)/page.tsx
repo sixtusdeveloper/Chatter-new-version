1e923ec03bea196161ec75c186ec617e06a4321b
sixtusdeveloper <sixtusushrey@gmail.com>
2024-12-08 18:30:44 +0100

    updated the navbar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -39,31 +39,24 @@ export default function Home() {
   };
 
   return (
-    <>
-      <SignedIn>
-        <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
-          <div className="max-w-7xl w-screen">
-            <NavBar navigation={navigation} />
-            <Hero />
-            <Features />
-            <Services onLinkClick={handleLinkClick} userId={userId} /> {/* Pass handleLinkClick */}
-            <Posts />
-            <Testimonials />
-            <Collaborators />
-            <FAQ />
-            <FAQFeedBack />
-            <Footer />
-            <Script
-              src="https://embed.tawk.to/66b0c4651601a2195ba0f54b/1i4h7ub94"
-              charset="UTF-8"
-              crossorigin="*"
-            />
-          </div>
-        </main>
-      </SignedIn>
-      <SignedOut>
-        <RedirectToSignIn />
-      </SignedOut>
-    </>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
+      <div className="max-w-7xl w-screen">
+        <NavBar navigation={navigation} />
+        <Hero />
+        <Features />
+        <Services onLinkClick={handleLinkClick} userId={userId} /> {/* Pass handleLinkClick */}
+        <Posts />
+        <Testimonials />
+        <Collaborators />
+        <FAQ />
+        <FAQFeedBack />
+        <Footer />
+        <Script
+          src="https://embed.tawk.to/66b0c4651601a2195ba0f54b/1i4h7ub94"
+          charset="UTF-8"
+          crossorigin="*"
+        />
+      </div>
+    </main>
   );
 }
